f80b4770f934f6e07050ffa410cfeceddf362a49
zuriscript <zur.shmaria@gmail.com>
2023-07-19 19:52:11 +0200

    some fixes for landing page and use correct github url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -31,7 +31,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/zuriscript/signalstory/tree/master/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
