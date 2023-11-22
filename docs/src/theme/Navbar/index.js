501855b3c30092ee4c1cb3b885fa95407ba6bc10
zuriscript <zur.shmaria@gmail.com>
2023-11-22 11:23:46 +0100

    Remove navbar shadow on small screen sizes if scrolled to top

--- docs/src/theme/Navbar/index.js
+++ docs/src/theme/Navbar/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
 import Navbar from '@theme-original/Navbar';
+import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function NavbarWrapper(props) {
   const location = useLocation();
   const isRootPath = location.pathname === '/signalstory/';
   const classTag = isRootPath ? 'landing-page-nav-sibling' : 'docs-nav-sibling';
+
+  React.useLayoutEffect(() => {
+    if (window.innerWidth < 997) {
+      const checkScrollPosition = () => {
+        document.body.classList.toggle('scrolling', window.scrollY > 0);
+      };
+      const timerId = setInterval(checkScrollPosition, 250);
+      return () => clearInterval(timerId);
+    }
+  }, []);
 
   return (
     <>
