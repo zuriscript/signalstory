5ba690e4a980a06e7928f6d6e9ce69e64502a87d
zuriscript <zur.shmaria@gmail.com>
2024-01-01 10:23:04 +0100

    refactor persistence tests

--- packages/signalstory/src/__tests__/store-plugin-persistence.spec.ts
+++ packages/signalstory/src/__tests__/store-plugin-persistence.spec.ts
@@ -1,23 +1,15 @@
 /* eslint-disable tree-shaking/no-side-effects-in-initialization */
-import { EnvironmentInjector } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { lastValueFrom, take } from 'rxjs';
 import { Store } from '../lib/store';
-import { PersistenceStorage } from '../lib/store-plugin-persistence/persistence-sync-storage';
+import { SyncStorage } from '../lib/store-plugin-persistence/persistence-sync-storage';
 import {
   clearStoreStorage,
-  loadFromStoreStorage,
-  saveToStoreStorage,
   useStorePersistence,
 } from '../lib/store-plugin-persistence/plugin-persistence';
-import { Cmp, registerAndGetStore } from './helper';
+import { registerAndGetStore } from './helper';
 
-describe('loadFromStorage', () => {
-  const initialValue = { val: 'initial' };
-  const key = 'myStoreKey';
-  let store: Store<{ val: string }>;
-  let storage: PersistenceStorage;
+describe('load from storage', () => {
+  let storage: SyncStorage;
+  const key = 'key';
 
   beforeEach(() => {
     storage = {
@@ -25,59 +17,76 @@ describe('loadFromStorage', () => {
       setItem: jest.fn(),
       removeItem: jest.fn(),
     };
-    store = registerAndGetStore({
-      initialState: initialValue,
+  });
+
+  it('should load the stored value from persistence storage on init', () => {
+    // arrange
+    const storedValue = { val: 'stored' };
+    storage.getItem = jest.fn(() => JSON.stringify(storedValue));
+
+    // act
+    const store = new Store({
+      initialState: { val: 'dummy' },
       plugins: [
         useStorePersistence({
-          persistenceKey: key,
           persistenceStorage: storage,
+          persistenceKey: key,
         }),
       ],
     });
-  });
-
-  it('should load the stored value from persistence storage', () => {
-    // arrange
-    storage.getItem = jest.fn(() => JSON.stringify(initialValue));
-
-    // act
-    const loadedValue = loadFromStoreStorage(store);
 
     // assert
-    expect(loadedValue).toStrictEqual(initialValue);
+    expect(store.state()).toStrictEqual(storedValue);
     expect(storage.getItem).toHaveBeenCalledWith(key);
   });
 
-  it('should return undefined when unable to parse the stored value', () => {
+  it('should not set initial state when unable to parse the stored value', () => {
     // arrange
+    const stateBeforeInit = { val: 'dummy' };
     storage.getItem = jest.fn(() => 'INVALID STATE');
 
     // act
-    const loadedValue = loadFromStoreStorage(store);
+    const store = new Store({
+      initialState: stateBeforeInit,
+      plugins: [
+        useStorePersistence({
+          persistenceStorage: storage,
+          persistenceKey: key,
+        }),
+      ],
+    });
 
     // assert
-    expect(loadedValue).toBeUndefined();
+    expect(store.state()).toStrictEqual(stateBeforeInit);
     expect(storage.getItem).toHaveBeenCalledWith(key);
   });
 
-  it('should return undefined when no stored value is available', () => {
+  it('should not set initial state when there is no stored value', () => {
     // arrange
+    const stateBeforeInit = { val: 'dummy' };
     storage.getItem = jest.fn(() => null);
 
     //act
-    const loadedValue = loadFromStoreStorage(store);
+    const store = new Store({
+      initialState: stateBeforeInit,
+      plugins: [
+        useStorePersistence({
+          persistenceStorage: storage,
+          persistenceKey: key,
+        }),
+      ],
+    });
 
     // assert
-    expect(loadedValue).toBeUndefined();
+    expect(store.state()).toStrictEqual(stateBeforeInit);
     expect(storage.getItem).toHaveBeenCalledWith(key);
   });
 });
 
-describe('saveToStorage', () => {
-  const initialValue = { val: 'initial' };
+describe('save to storage', () => {
   const key = 'myStoreKey';
   let store: Store<{ val: string }>;
-  let storage: PersistenceStorage;
+  let storage: SyncStorage;
 
   beforeEach(() => {
     storage = {
@@ -86,7 +95,7 @@ describe('saveToStorage', () => {
       removeItem: jest.fn(),
     };
     store = registerAndGetStore({
-      initialState: initialValue,
+      initialState: { val: 'initial' },
       plugins: [
         useStorePersistence({
           persistenceKey: key,
@@ -96,23 +105,47 @@ describe('saveToStorage', () => {
     });
   });
 
-  it('should save the store state to persistence storage', () => {
+  it('should save the store state to persistence storage on set', () => {
+    // arrange
+    const newValue = { val: 'newValue' };
+
     // act
-    saveToStoreStorage(store, store.state());
+    store.set(newValue);
 
     // assert
-    expect(storage.setItem).toHaveBeenCalledWith(
-      key,
-      JSON.stringify(store.state())
-    );
+    expect(storage.setItem).toHaveBeenCalledWith(key, JSON.stringify(newValue));
+  });
+
+  it('should save the store state to persistence storage on update', () => {
+    // arrange
+    const newValue = { val: 'newValue' };
+
+    // act
+    store.update(() => newValue);
+
+    // assert
+    expect(storage.setItem).toHaveBeenCalledWith(key, JSON.stringify(newValue));
+  });
+
+  it('should save the store state to persistence storage on mutate', () => {
+    // arrange
+    let newValue: { val: string } | undefined;
+
+    // act
+    store.mutate(state => {
+      state.val = 'newValue';
+      newValue = state;
+    });
+
+    // assert
+    expect(storage.setItem).toHaveBeenCalledWith(key, JSON.stringify(newValue));
   });
 });
 
 describe('clearStorage', () => {
-  const initialValue = { val: 'initial' };
   const key = 'myStoreKey';
   let store: Store<{ val: string }>;
-  let storage: PersistenceStorage;
+  let storage: SyncStorage;
 
   beforeEach(() => {
     storage = {
@@ -121,7 +154,7 @@ describe('clearStorage', () => {
       removeItem: jest.fn(),
     };
     store = registerAndGetStore({
-      initialState: initialValue,
+      initialState: { val: 'initial' },
       plugins: [
         useStorePersistence({
           persistenceKey: key,
@@ -137,68 +170,5 @@ describe('clearStorage', () => {
 
     // assert
     expect(storage.removeItem).toHaveBeenCalledWith(key);
-  });
-});
-
-describe('automatic sync', () => {
-  const initialValue = { val: 'initial' };
-  const initialValueFromStorage = { val: 'initialFromStorage' };
-  const key = 'myStoreKey';
-  let store!: Store<{ val: string }>;
-  let storage!: PersistenceStorage;
-  let fixture!: ComponentFixture<unknown>;
-  let injector!: EnvironmentInjector;
-
-  function flushEffects(): void {
-    fixture.detectChanges();
-  }
-
-  beforeEach(() => {
-    storage = {
-      getItem: jest.fn(),
-      setItem: jest.fn(),
-      removeItem: jest.fn(),
-    };
-    storage.getItem = jest.fn(() => JSON.stringify(initialValueFromStorage));
-    store = registerAndGetStore({
-      initialState: initialValue,
-      plugins: [
-        useStorePersistence({
-          persistenceKey: key,
-          persistenceStorage: storage,
-        }),
-      ],
-    });
-    fixture = TestBed.createComponent(Cmp);
-    injector = TestBed.inject(EnvironmentInjector);
-  });
-
-  it('Should initialize from storage', () => {
-    // assert
-    expect(store.state()).toStrictEqual(initialValueFromStorage);
-    expect(loadFromStoreStorage(store)).toStrictEqual(initialValueFromStorage);
-  });
-
-  it('should persist new state', async () => {
-    // arrange
-    // Clean prior effect emits and storage applications
-    flushEffects();
-    storage.setItem = jest.fn();
-    const newState = { val: 'newState' };
-    const storeChange = lastValueFrom(
-      toObservable(store.state, { injector }).pipe(take(1))
-    );
-
-    store.set(newState, 'blbal');
-    // emits last store change
-    flushEffects();
-
-    // act
-    const storeChangeResult = await storeChange;
-
-    // assert
-    expect(storeChangeResult).toEqual(newState);
-    expect(storage.setItem).toHaveBeenCalledTimes(1);
-    expect(storage.setItem).toHaveBeenCalledWith(key, JSON.stringify(newState));
   });
 });
